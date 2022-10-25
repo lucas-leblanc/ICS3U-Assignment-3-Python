62dc5d1302b3025d0ed1fd0fb2d48adf1131bb45
Lucas LeBlanc <lucas.leblanc@mths.ca>
2022-10-25 14:54:18 +0000

    added file

--- triangle_teller.py
+++ triangle_teller.py
@@ -11,10 +11,10 @@ y = int(input("y: "))
 z = int(input("z: "))
 
 if x == y == z:
-	print("Equilateral triangle")
-elif x==y or y==z or z==x:
-	print("isosceles triangle")
+    print("Equilateral triangle")
+elif x == y or y == z or z == x:
+    print("isosceles triangle")
 else:
-	print("Scalene triangle")
+    print("Scalene triangle")
 
 print("\n\nDone.")
